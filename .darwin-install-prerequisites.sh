9669deefb05319c2743740a9e51782080373ff32
Itai Koren <itkoren@gmail.com>
2025-01-02 15:15:06 -0800

    Update .darwin-install-prerequisites.sh

--- .darwin-install-prerequisites.sh
+++ .darwin-install-prerequisites.sh
@@ -1,22 +1,46 @@
 #!/bin/bash
 set -ex
 
-if type op >/dev/null 2>&1; then
-    echo "1Password CLI is already installed"
-else
-    case "$(uname -s)" in
-    Darwin)
-        brew tap "1password/tap"
-        brew install --cask --force 1password
-        brew install --force 1password-cli
-        brew install --force git-delta
-        ;;
-    *)
-        echo "unsupported OS"
-        exit 1
-        ;;
-    esac
-fi
+function install_prerequisite() {
+    local prerequisite = "$1"
+    local is_tap = "$2"
+    local is_cask = "$3"
+
+    if [ -n "$is_tap" ]; then
+        brew tap "$prerequisite"
+    elif [ -n "$is_cask" ]; then
+        if ! brew list --cask "$prerequisite" > /dev/null 2>&1; then
+            brew install --cask "$prerequisite"
+        else
+            read -p "$prerequisite is already installed, would you like to re-install it? (y/n): " yn
+            if [[ "$yn" =~ ^[Yy]$ ]]; then
+                brew install --cask --force "$prerequisite"
+            fi
+        fi
+    else
+        if ! brew list "$prerequisite" > /dev/null 2>&1; then
+            brew install "$prerequisite"
+        else
+            read -p ""$prerequisite" is already installed, would you like to re-install it? (y/n): " yn
+            if [[ "$yn" =~ ^[Yy]$ ]]; then
+                brew install --force "$prerequisite"
+            fi
+        fi
+    fi
+}
+
+case "$(uname -s)" in
+Darwin)
+    install_prerequisite "1password/tap" "true"
+    install_prerequisite "1password" "" "true"
+    install_prerequisite "1password-cli"
+    install_prerequisite "git-delta"
+    ;;
+*)
+    echo "unsupported OS"
+    exit 1
+    ;;
+esac
 
 echo "Please open 1Password, log into all accounts and set the following:"
 echo " - Settings>Security>Unlock activate Touch ID"
