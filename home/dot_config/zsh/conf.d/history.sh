6e7bea1b0c32a9d7da8c9a6e7c186bfe6afd1338
Itai Koren <itkoren@gmail.com>
2025-01-01 22:57:34 -0800

    Update history.sh

--- home/dot_config/zsh/conf.d/history.sh
+++ home/dot_config/zsh/conf.d/history.sh
@@ -1,2 +1,6 @@
-export HISTSIZE=100000000
-export SAVEHIST=100000000
+#!/bin/bash
+
+# Keep 10,000,000 lines of history within the shell and save it to ~/.zsh_history:
+export HISTSIZE=10000000
+export SAVEHIST=10000000
+export HISTFILE=${ZDOTDIR:-$HOME}/.zsh_history
