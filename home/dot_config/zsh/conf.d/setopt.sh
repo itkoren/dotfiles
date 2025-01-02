5c1934ca1c1677bac39175b468e22acdc2decf83
Itai Koren <itkoren@gmail.com>
2025-01-01 22:59:52 -0800

    Update setopt.sh

--- home/dot_config/zsh/conf.d/setopt.sh
+++ home/dot_config/zsh/conf.d/setopt.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+setopt NO_CASE_GLOB       # Globbing and tab-completion to be case-insensitive.
+setopt GLOB_COMPLETE
+setopt AUTO_CD            # Enable changing to directories without typing cd first.
+
+setopt CORRECT            # enable command auto-correction
+setopt CORRECT_ALL
