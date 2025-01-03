8c1c7c7ac8e50e56f88a5564011271369c3d2c1c
Itai Koren <itkoren@gmail.com>
2025-01-03 03:41:06 -0800

    Update .darwin-install-prerequisites.sh

--- .darwin-install-prerequisites.sh
+++ .darwin-install-prerequisites.sh
@@ -27,6 +27,9 @@ Darwin)
     brewfile_content+=$(generate_brewfile "1password-cli" "" "")
     brewfile_content+=$(generate_brewfile "git-delta" "" "")
 
+    # Ensure the content is properly separated by newlines
+    brewfile_content=$(echo "$brewfile_content" | sed 's/$/\n/')
+    
     # Run brew bundle with the generated Brewfile content
     brew bundle --no-lock --file=/dev/stdin <<< "$brewfile_content"
     ;;
