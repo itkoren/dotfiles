f41e495beb0286d1beebdea17a0afd7a97ecc66d
Itai Koren <itkoren@gmail.com>
2025-01-06 08:37:04 +0200

    Update dot_fancy_echo.sh

--- home/dot_config/zsh/conf.d/functions/dot_fancy_echo.sh
+++ home/dot_config/zsh/conf.d/functions/dot_fancy_echo.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-fancy_echo() {
+function fancy_echo {
   local fmt="$1"; shift
 
   # shellcheck disable=SC2059
   printf "\n$fmt\n" "$@"
 }
 
-fancy_echo_line() {
+function fancy_echo_line {
   local fmt="$1"; shift
 
   # shellcheck disable=SC2059
