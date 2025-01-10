ca43b0b3a2287fd9e34597729b486bdf9274dc19
Itai Koren <itkoren@gmail.com>
2025-01-10 00:01:24 -0800

    Update .darwin-install-prerequisites.sh

--- .darwin-install-prerequisites.sh
+++ .darwin-install-prerequisites.sh
@@ -55,6 +55,7 @@ Darwin)
     install_prerequisite "1password" "" 1
     install_prerequisite "1password-cli" "" ""
     install_prerequisite "git-delta" "" ""
+    install_prerequisite "age" "" 1
     ;;
 *)
     echo "unsupported OS"
