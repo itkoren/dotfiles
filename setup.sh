537f6aa74538b6faccb7c12d5668204be879493a
Itai Koren <itkoren@gmail.com>
2025-01-12 07:56:30 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -203,16 +203,6 @@ function initialize_os_macos() {
             echo "Setting active developer directory to Xcode..."
             sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
         fi
-    
-        # Accept the Xcode license if not already accepted
-        if sudo xcodebuild -license check &>/dev/null; then
-            echo "Xcode license already accepted."
-        else
-            echo "Xcode license has not been accepted."
-            echo "Accepting the Xcode license..."
-            sudo xcodebuild -license accept
-            echo "Xcode license accepted."
-        fi
     else
         # If only Command Line Tools are installed, handle accepting the license
         if [ -d "/Library/Developer/CommandLineTools" ]; then
@@ -221,14 +211,17 @@ function initialize_os_macos() {
                 echo "Setting active developer directory to Command Line Tools..."
                 sudo xcode-select --switch /Library/Developer/CommandLineTools
             fi
-    
-            # Accept the CLT license using xcode-select
-            if sudo xcode-select --install &>/dev/null; then
-                echo "Command Line Tools license accepted."
-            else
-                echo "Unable to accept the Command Line Tools license automatically."
-            fi
         fi
+    fi
+
+    # Accept the Xcode license if not already accepted
+    if sudo xcodebuild -license check &>/dev/null; then
+        echo "Xcode license already accepted."
+    else
+        echo "Xcode license has not been accepted."
+        echo "Accepting the Xcode license..."
+        sudo xcodebuild -license accept
+        echo "Xcode license accepted."
     fi
 
     # Verify dictation shortcut 
