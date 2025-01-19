81cf13b4b801a8936feff11e313dac35ba9c4ad3
Itai Koren <itkoren@gmail.com>
2025-01-19 12:29:22 -0800

    Update .linux-install-prerequisites.sh

--- .linux-install-prerequisites.sh
+++ .linux-install-prerequisites.sh
@@ -1,11 +1,15 @@
-case "$(uname -s)" in
-Linux*)
-    nix-env -iA nixpkgs.1password-cli
-    nix-env -iA nixpkgs.git-delta
-    nix-env -iA nixpkgs.age
-    ;;
-*)
-    echo "unsupported OS"
-    exit 1
-    ;;
-esac
+if type op >/dev/null 2>&1; then
+    echo "1Password CLI is already installed"
+else
+    case "$(uname -s)" in
+    Linux*)
+        nix-env -iA nixpkgs.1password-cli
+        nix-env -iA nixpkgs.git-delta
+        nix-env -iA nixpkgs.age
+        ;;
+    *)
+        echo "unsupported OS"
+        exit 1
+        ;;
+    esac
+fi
